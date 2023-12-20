ac16cebe2323a219a6ece522a47589aa9e33003a
James L Mueller <jamesmueller@liongatetechnology.com>
2023-12-20 06:35:07 -0600

    tabs

---
+++ app/(tabs)/_layout.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+
+export default function TabLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen name="home" />
+      <Tabs.Screen name="list" />
+    </Tabs>
+  );
+}
---
+++ app/index.js
@@ -0,0 +1,6 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Redirect } from "expo-router";
+
+export default function Page() {
+  return <Redirect href="/home" />;
+}
